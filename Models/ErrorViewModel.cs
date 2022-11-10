5a80d907a7fe9130f283f136c7ce5f016c4a84d8
Noel Lopes <noel@ipg.pt>
2022-11-10 17:06:07 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace CarManufactoring.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
