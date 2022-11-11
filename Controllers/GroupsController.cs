86b9e016609abe22795e4063997e039131fe7796
Noel Lopes <noel@ipg.pt>
2022-11-11 11:29:28 +0000

    Add groups page

---
+++ Controllers/GroupsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarManufactoring.Controllers {
+    public class GroupsController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        // TODO: Each group should add a page to show their group information and status
+    }
+}
