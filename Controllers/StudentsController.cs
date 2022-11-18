ea364f05c3a444ea0cd334d85116d02d3e734bbc
Noel Lopes <noel@ipg.pt>
2022-11-18 12:00:09 +0000

    Change the details page for students

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -13,61 +13,7 @@ namespace CarManufactoring.Controllers {
 
         // GET: Students/Details
         public IActionResult Details(string number) {
-            return View();
+            return View($"Details{number}");
         }
-
-        //get: Students/AnaVidalDetails
-        public IActionResult AnaVidalDetails() {
-            return View();
-        }
-
-        //get: Students/JoaoAleixoDetails
-        public IActionResult JoaoAleixoDetails() {
-            return View();
-        }
-
-        public IActionResult RodrigoLourencoDetails() {
-            return View();
-        }
-
-        public IActionResult LuisBarrosDetails() { return View(); }
-
-        public IActionResult MustafaBukhariDetails() {
-            return View();
-        }
-
-        public IActionResult TomasEstevesDetails() {
-            return View();
-        }
-
-        public IActionResult RicardoSousaDetails() {
-            return View();
-        }
-
-        public IActionResult RicardoAndradeDetails() {
-            return View();
-        }
-
-        public IActionResult PauloProencaDetails() {
-            return View();
-        }
-
-        public IActionResult RuiCondessoDetails() {
-            return View();
-        }
-        public IActionResult PedroMatosDetails() {
-            return View();
-        }
-        public IActionResult GuilhermeAlvesDetails() {
-            return View();
-        }
-        public IActionResult TelmoMoraisDetails() {
-            return View();
-        }
-        public IActionResult FabioAbreuDetails() {
-            return View();
-        }
-
-
     }
 }
