6e653a5618ea3686fd915bf6a9f20900b99ad5ae
Noel Lopes <noel@ipg.pt>
2022-11-24 11:18:14 +0000

    Created collaborators module

---
+++ Data/CarManufactoringContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CarManufactoring.Models;
+
+namespace CarManufactoring.Data
+{
+    public class CarManufactoringContext : DbContext
+    {
+        public CarManufactoringContext (DbContextOptions<CarManufactoringContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CarManufactoring.Models.Collaborator> Collaborator { get; set; } = default!;
+    }
+}
---
+++ Models/Collaborator.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarManufactoring.Models {
+    public class Collaborator {
+
+        public int CollaboratorId { get; set; }
+
+        [StringLength(100,MinimumLength = 10 ) ]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(256)]
+        public string Email { get; set; }
+
+        [Required]
+        public bool Available { get; set; }
+
+    }
+}
