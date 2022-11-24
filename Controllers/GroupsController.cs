8701ca94154c68b8ef89e0e17cabee75a5538120
rda <ricardo.dias.andrade@gmail.com>
2022-11-24 08:58:47 +0000

    Update some files to avoid merge conflict with master branch

--- Controllers/GroupsController.cs
+++ Controllers/GroupsController.cs
@@ -9,48 +9,8 @@ namespace CarManufactoring.Controllers {
             return View(groups);
         }
         //get: Groups/Grupo6details
-        public IActionResult Grupo6details()
-        {
-            return View();
-        }
-
-        public IActionResult Group1Details()
-        {
-            return View();
-        }
-        
-        public IActionResult Group5Details()
-        {
-            return View();
-        }
-
-        // TODO: Each group should add a page to show their group information and status
-
-
-        public IActionResult Group7Details()
-        {
-          return View();
-        }
-        public IActionResult Group2Details() {
-
-            return View();
-        }
-        public IActionResult Group3Details()
-        {
-
-            return View();
-        }
-
-        public IActionResult Group9details()
-        {
-
-            return View();
-        }
-
-        //get: Groups/Group4details
-        public IActionResult Group4Details()
-        {
-            return View();
+        public IActionResult Details(string number) {
+            return View($"Details{number}");
         }
 
     }
