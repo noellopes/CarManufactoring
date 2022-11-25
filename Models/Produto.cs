cc580bbc4f92d5c192914c2266bd8a4955cc4fa6
1705258 <1705258@SALA37-12>
2022-11-25 12:03:28 +0000

    Created Product class as a base for car pieces and raw material

---
+++ Models/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarManufactoring.Models {
+    public class Produto {
+
+        public int ProdutoId {get; set;}
+
+        [Required]
+        [StringLength(9,MinimumLength =3)]
+        public string Reference { get; set; }
+
+        [Required]
+        [StringLength(120, MinimumLength =4)]
+        public string Name { get; set; }
+
+        [Required]
+        public int PointOfPurchase { get; set; }
+
+        [Required]
+        public int SafetyStock { get; set; }
+
+        [Required]
+        [Range(0,1)]
+        public decimal LevelService { get; set; }
+
+        [Required]
+        public string StockState { get; set; } //String for now, will later change to it's corresponding state type
+
+        //Methods (will be added later)
+    }
+}
