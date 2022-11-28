1527f6181ca1d1fc4ba6d861f17500a7321dcd8f
Tomás Esteves <tomsesteves@gmail.com>
2022-11-28 12:10:32 +0000

    Actualização de tarefas realizadas na minha página pessoal

--- ViewModels/Group1/Group1Documents.cs
+++ ViewModels/Group1/Group1Documents.cs
@@ -12,6 +12,8 @@
         public readonly static List<Documents> TomasDocuments = new List<Documents>() {
              new Documents {Name = "Descrição Caso Uso Finalizar Trabalho  ", DocPath="/documents/grupo1/tomas_esteves/diagramas/descricao_caso_finalizar_trabalho_Tomás_Esteves_1704696.pdf"},
              new Documents {Name = "Diagrama Sequência Criar Tarefa  ", DocPath="/documents/grupo1/tomas_esteves/diagramas/diagrama_sequênica_classe_Tomás_Esteves_1704696V2.drawio.pdf"},
+             new Documents {Name = "Diagrama Semântica \"Trabalho Manutenção\"  ", DocPath="/documents/grupo1/tomas_esteves/diagramas/Semântica de Classe_Tomás_Esteves_1704696.pdf"},
+             new Documents {Name = "História Utilizador \"Responsável\"", DocPath="/documents/grupo1/tomas_esteves/diagramas/historia_utilizador_tomas_esteves_1704696.pdf"},
         };
 
         public readonly static List<Documents> DiogoDocuments = new List<Documents>() {
