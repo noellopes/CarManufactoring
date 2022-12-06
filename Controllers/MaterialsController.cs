59c9a8d852225d7654688c0684778611ca4b93a1
franciscop101 <83250815+franciscop101@users.noreply.github.com>
2022-12-06 18:09:43 +0000

    criacao da tabela material

---
+++ Controllers/MaterialsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CarManufactoring.Data;
+using CarManufactoring.Models;
+
+namespace CarManufactoring.Controllers
+{
+    public class MaterialsController : Controller
+    {
+        private readonly CarManufactoringContext _context;
+
+        public MaterialsController(CarManufactoringContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Materials
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Material.ToListAsync());
+        }
+
+        // GET: Materials/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Material == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Material
+                .FirstOrDefaultAsync(m => m.MaterialId == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            return View(material);
+        }
+
+        // GET: Materials/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Materials/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaterialId,Nome,Description,State,Type")] Material material)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(material);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(material);
+        }
+
+        // GET: Materials/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Material == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Material.FindAsync(id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+            return View(material);
+        }
+
+        // POST: Materials/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaterialId,Nome,Description,State,Type")] Material material)
+        {
+            if (id != material.MaterialId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(material);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MaterialExists(material.MaterialId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(material);
+        }
+
+        // GET: Materials/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Material == null)
+            {
+                return NotFound();
+            }
+
+            var material = await _context.Material
+                .FirstOrDefaultAsync(m => m.MaterialId == id);
+            if (material == null)
+            {
+                return NotFound();
+            }
+
+            return View(material);
+        }
+
+        // POST: Materials/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Material == null)
+            {
+                return Problem("Entity set 'CarManufactoringContext.Material'  is null.");
+            }
+            var material = await _context.Material.FindAsync(id);
+            if (material != null)
+            {
+                _context.Material.Remove(material);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MaterialExists(int id)
+        {
+          return _context.Material.Any(e => e.MaterialId == id);
+        }
+    }
+}
