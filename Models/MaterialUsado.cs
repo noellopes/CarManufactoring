59b643d5a4aeb51d02d1546d1e1d9c491d4a9e6a
Ameacaa <7kevinla@gmail.com>
2022-12-09 11:20:12 +0000

    Criação de classe material usado

---
+++ Models/MaterialUsado.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarManufactoring.Models
+{
+    public class MaterialUsado
+    {
+        public int MaterialId { get; set; }
+        public int SemiFinishedId { get; set; }
+
+        [Required]
+        public int QuantidadeNecessaria { get; set; }
+        
+    }
+
+}
+
