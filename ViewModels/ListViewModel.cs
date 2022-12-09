5244895c5d7ed7adc77326c656166a78e196e99d
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-12-09 12:23:32 +0000

    worcking in pagination, it is not showing the buttons

---
+++ ViewModels/ListViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarManufactoring.ViewModels
+{
+    public class ListViewModel<T>
+    {
+        public IEnumerable<T> List { get; set; }
+        public PagingInfoViewModel PagingInfo { get; set; }
+    }
+}
---
+++ ViewModels/SemiFinishedIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CarManufactoring.Models;
+
+namespace CarManufactoring.ViewModels
+{
+    public class SemiFinishedIndexViewModel
+    {
+        public ListViewModel<SemiFinished> SemiFinishedList { get; set; }
+        
+        public string? FamilySearched { get; set; }
+        
+        public string? ReferenceSearched { get; set; }
+        
+        public string? EANSearched { get; set; }
+        
+        public string? ManufacterSearched { get; set; }
+
+        public string? SemiFinishedStateSearched { get; set; }
+    }
+}
