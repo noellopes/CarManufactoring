87f089b1a66316bc520e6d28cd85af80b973a595
rda <ricardo.dias.andrade@gmail.com>
2022-12-13 10:33:49 +0000

    Updated Work details Updated files Updated Group details

--- Models/Product.cs
+++ Models/Product.cs
@@ -31,7 +31,9 @@ namespace CarManufactoring.Models {
 
         public CarParts SearchProd(int ProductId, CarManufactoringContext context, string NameSearch, string TypeSearch, string referenceSearch) {
 
-            var carPart = context.CarParts.Where(cp => cp.Name == NameSearch || cp.PartType == TypeSearch || cp.reference ==referenceSearch).OrderBy(cp => cp.Name);
+            CarParts carPart = (CarParts) context.CarParts
+                .Where(cp => cp.Name == NameSearch || cp.PartType == TypeSearch || cp.Reference ==referenceSearch)
+                .OrderBy(cp => cp.Name);
 
             return carPart;
 
