36c50c4f1e6cd6183a0a4b193235debdefee85f0
Luís Barros <luisantoniio1998@gmail.com>
2022-12-14 17:33:05 +0000

    Start trying to run the program before merging it

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -29,7 +29,7 @@ namespace CarManufactoring.Data
             PopulateOrder(db);
             PopulateMaterialUsado(db);
             //PopulateSupplier(db);
-            PopulateStocks(db);
+           //PopulateStocks(db);
             PopulateExtras(db);
             PopulateCollaborators(db);
         }
