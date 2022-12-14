68829e0a3bf9dae9bd3357261e0def6d2458329e
osmancanofraz <117988839+osmancanofraz@users.noreply.github.com>
2022-12-15 02:17:43 +0300

    added classes for workıng hours and attended hours

---
+++ Models/AttendedHours.cs
@@ -0,0 +1,18 @@
+﻿namespace CarManufactoring.Models
+{
+    public class AttendedHours
+    {
+        public int AttendedHoursId { get; set; }
+        public string? WorkerName { get; set; }
+        public int WorkedHour { get; set; }
+        public DateTime CheckedIn { get; set; }
+        public DateTime CheckedOut { get; set; }
+
+        public string? workingStatus { get; set; }
+        public bool IsAttended { get; set; }
+
+
+
+    }
+
+}
