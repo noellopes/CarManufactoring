21088d8f62fe2ea0598e246f8908840113085fed
mustafabukh <62837072+mustafabukh@users.noreply.github.com>
2022-12-14 21:00:11 +0000

    added collaborato seedData

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -400,7 +400,9 @@ namespace CarManufactoring.Data
         {
             if (db.Collaborator.Any()) return;
             db.Collaborator.AddRange(
-                new Collaborator { Name = "test", BirthDate = DateTime.Parse("12 / 12 / 1999"), Phone = "919293949", Email = "test@email.pt", GenderId = 1 }
+                new Collaborator { Name = "Worker1", BirthDate = DateTime.Parse("12 / 12 / 1999"), Phone = "919293949", Email = "Worker1@cars.pt", GenderId = 1, OnDuty = true },
+                new Collaborator { Name = "Worker1", BirthDate = DateTime.Parse("12 / 12 / 1995"), Phone = "919293949", Email = "Worker3@cars.pt", GenderId = 2, OnDuty = true },
+                new Collaborator { Name = "Worker1", BirthDate = DateTime.Parse("12 / 12 / 1991"), Phone = "919293949", Email = "Worker3@cars.pt", GenderId = 1, OnDuty = false }
                 );
             db.SaveChanges();
         }
