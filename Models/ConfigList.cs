8283a9f975db61a6844d694b09293afe09ee3fef
RodrigoSL06 <rls.lourenco@hotmail.com>
2022-12-14 16:25:35 +0000

    Relation Between carConfig and extra (N:N)

---
+++ Models/ConfigList.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarManufactoring.Models
+{
+    public class ConfigList
+    {
+       
+        public int ConfigListId { get; set; }
+
+        [Required]
+        public int CarConfigId { get; set; }
+        public CarConfig? CarConfig { get; set; }
+
+        [Required]
+        public int ExtraId { get; set; }
+        public Extra? Extra { get; set; }
+
+
+
+    }
+}
