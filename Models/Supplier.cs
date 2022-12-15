ded4367d8a11a722de07b5626ac8993fbf1ef579
rafaela lopes <rafaelarodrigues747@gmail.com>
2022-12-15 10:42:46 +0000

    Supplier

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -22,7 +22,6 @@ namespace CarManufactoring.Models
         [Required(ErrorMessage = "Please enter the contact")]
         [Display(Name = "Phone Number")]
         [StringLength(9, MinimumLength = 9, ErrorMessage = "The contact must have 9 characters")]
-        [RegularExpression(@"(9\d{8})", ErrorMessage = "invalid number.")]
         [Phone]
         public string SupplierContact { get; set; }
 
