dcb6e35640cefef449f8475bd7a51659b3afb351
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-12-15 21:43:37 +0000

    Alteracao no modelo e no index

--- Models/SemiFinishedCar.cs
+++ Models/SemiFinishedCar.cs
@@ -7,16 +7,6 @@ namespace CarManufactoring.Models
         [Key]
         public int SemiFinishedCarId { get; set; }
 
-        public int Name { get; set; }
-
-        public int Reference { get; set; }
-
-        public int Brand { get; set; }
-
-        public int Model { get; set; }
-
-        public int Year { get; set; }
-
         public int? SemiFinishedId { get; set; }
         public SemiFinished? SemiFinished { get; set; }
 
