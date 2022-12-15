7b98f5e57a149cd68dfd2506bc9a4f33e69d1601
Fabio Abreu <fabiofiqueli@hotmail.com>
2022-12-15 20:48:55 +0000

    scaffolding relacao many to many

--- Models/SemiFinishedCar.cs
+++ Models/SemiFinishedCar.cs
@@ -7,10 +7,20 @@ namespace CarManufactoring.Models
         [Key]
         public int SemiFinishedCarId { get; set; }
 
-        public int SemiFinishedId { get; set; }
+        public int Name { get; set; }
+
+        public int Reference { get; set; }
+
+        public int Brand { get; set; }
+
+        public int Model { get; set; }
+
+        public int Year { get; set; }
+
+        public int? SemiFinishedId { get; set; }
         public SemiFinished? SemiFinished { get; set; }
 
-        public int CarId { get; set; }
+        public int? CarId { get; set; }
         public Car? Car { get; set;}
     }
 }
