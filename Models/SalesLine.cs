2f51fc6d4df33350a189fe49defd3574b4e967f6
pedromatos98 <matospedro1998@gmail.com>
2022-12-16 10:53:05 +0000

    created class SalesLine

---
+++ Models/SalesLine.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarManufactoring.Models
+{
+    public class SalesLine
+    {
+        public int SalesLineId { get; set; }
+        public int OrderId { get; set; }
+        public Order? Order{ get; set; }
+
+        public int CarId { get; set; }
+        public Car? Car { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double Price { get; set; }
+    }
+}
