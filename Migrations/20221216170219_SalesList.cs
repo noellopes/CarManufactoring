6a9043503cddfd18e2ac0591237bc96628256dc2
pedromatos98 <matospedro1998@gmail.com>
2022-12-16 17:06:03 +0000

    criada migração SalesLine

---
+++ Migrations/20221216170219_SalesList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarManufactoring.Migrations
+{
+    public partial class SalesList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SalesLine",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    CarConfigId = table.Column<int>(type: "int", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    DeliveryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesLine", x => new { x.OrderId, x.CarConfigId });
+                    table.ForeignKey(
+                        name: "FK_SalesLine_CarConfig_CarConfigId",
+                        column: x => x.CarConfigId,
+                        principalTable: "CarConfig",
+                        principalColumn: "CarConfigId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SalesLine_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Order",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesLine_CarConfigId",
+                table: "SalesLine",
+                column: "CarConfigId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesLine");
+        }
+    }
+}
