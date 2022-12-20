29b797465f24f1467071c591fdb828123f9996bb
Luís Barros <luisantoniio1998@gmail.com>
2022-12-20 16:47:07 +0000

    Uploaded file done  in Software Engineering and Showing it on personal page

--- ViewModels/Group3/Group3Documents.cs
+++ ViewModels/Group3/Group3Documents.cs
@@ -16,6 +16,7 @@ namespace CarManufactoring.ViewModels.Group3
 
         public readonly static List<Documents> LuisDocuments = new List<Documents>()
         {
+            new Documents {Name = "Traceability's matrix", DocPath="/documents/Group3/LuisBarros/MatrizRastreabilidade.pdf"},
             new Documents {Name ="Tests Table", DocPath = "/documents/Group3/LuisBarros/TabelaTestes.pdf"},
             new Documents {Name = "User Stories", DocPath= "/documents/Group3/LuisBarros/UserStories.pdf"},
             new Documents {Name = "Order Class Semantics Diagram Updated", DocPath= "/documents/Group3/LuisBarros/SemanticaClasseAtualizada.pdf"},
