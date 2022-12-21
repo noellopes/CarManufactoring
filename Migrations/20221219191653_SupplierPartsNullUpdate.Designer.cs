04d0cc01957f8626ac5025443ec611c674459bed
Filipe Santos <44101477+NobleLip@users.noreply.github.com>
2022-12-21 14:31:59 +0000

    Atualizados ficheiros antes de começar a fazer tabelas novas, pesquisa e paginação

--- Migrations/20221219191653_SupplierPartsNullUpdate.Designer.cs
+++ Migrations/20221219191653_SupplierPartsNullUpdate.Designer.cs
@@ -4,6 +4,7 @@ using CarManufactoring.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarManufactoring.Migrations
 {
     [DbContext(typeof(CarManufactoringContext))]
-    partial class CarManufactoringContextModelSnapshot : ModelSnapshot
+    [Migration("20221219191653_SupplierPartsNullUpdate")]
+    partial class SupplierPartsNullUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/SupplierParts.cs
+++ Models/SupplierParts.cs
@@ -9,13 +9,13 @@ namespace CarManufactoring.Models
 
         public int SupplierPartsId { get; set; }
 
-
-
+        [Display(Name = "Logo")]
+        [Url]
+        public string? Logo { get; set; }
 
         [Required(ErrorMessage = "Please enter the Name")]
         [Display(Name = "Name")]
         [StringLength(100, MinimumLength = 2)]
-
         public string Name { get; set; }
 
 
@@ -24,7 +24,6 @@ namespace CarManufactoring.Models
         [Required(ErrorMessage = "Please enter the Email")]
         [EmailAddress]
         [StringLength(256)]
-
         public string Email { get; set; }
 
 
@@ -33,25 +32,24 @@ namespace CarManufactoring.Models
         [Required(ErrorMessage = "Please enter the contact")]
         [Display(Name = "Phone Number")]
         [StringLength(20, MinimumLength = 9)]
-
         public string Contact { get; set; }
-
-
- 
 
 
         [Required]
         [Display(Name = "ZIP Code")]
         [StringLength(8)]
-
         public string ZipCode { get; set; }
  
 
         [Required]
         [Display(Name = "Address")]
         [StringLength(100, MinimumLength = 2)]
-
         public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Country")]
+        [StringLength(100, MinimumLength = 2)]
+        public string Country { get; set; }
     }
 }
 
