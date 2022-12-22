e3dbd5e777a68a632086678a53f9a0402476ddd2
Condesso1998 <ruicondesso98@gmail.com>
2022-12-22 18:50:55 +0000

    add class  StockFinalProduct  and rename Localization to localizationCar

---
+++ Models/StockFinalProduct.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+
+namespace CarManufactoring.Models
+{
+    public class StockFinalProduct
+    {
+
+        public int LocalizationCarId { get; set; }
+        public LocalizationCar? LocalizationCar { get; set; }
+
+        public int CarConfigId { get; set; }
+        public CarConfig? CarConfig { get; set; }
+
+        [Required]
+        
+        public string ChassiNumber { get; set; }
+
+        public int  ProductionId { get; set; }
+
+        public Production? Production { get; set; }
+
+
+    }
+}
