150a09a757e007b4ea8bdd13ace535d5cbde23b1
Ricardo <satao.net@hotmail.com>
2022-12-29 14:58:47 +0000

    Fixed an error that did not allow inserting data in the TimeOfProduction table.

---
+++ Migrations/20221229142102_CorrectionTimeOfProduction.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarManufactoring.Migrations
+{
+    public partial class CorrectionTimeOfProduction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TimeOfProduction_CarConfigId",
+                table: "TimeOfProduction");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeOfProduction_CarConfigId",
+                table: "TimeOfProduction",
+                column: "CarConfigId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TimeOfProduction_CarConfigId",
+                table: "TimeOfProduction");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeOfProduction_CarConfigId",
+                table: "TimeOfProduction",
+                column: "CarConfigId");
+        }
+    }
+}
