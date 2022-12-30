892b1a8128c0da18eb6636396d4f928111706a45
Condesso1998 <ruicondesso98@gmail.com>
2022-12-30 03:23:19 +0000

    StockFinalProductIndex View Model

--- Models/StockFinalProduct.cs
+++ Models/StockFinalProduct.cs
@@ -5,11 +5,15 @@
         public int StockFinalProductId { get; set; }
 
         public int LocalizationCarId { get; set; }
-        public LocalizationCar LocalizationCar { get; set; }
+
+        public LocalizationCar? LocalizationCar { get; set; }
 
         public string ChassiNumber { get; set; }
 
         public int ProductionId { get; set; }
-        public Production Production { get; set; }
+
+        public Production? Production { get; set; }
+
+        public DateTime InsertionDate { get; set; }
     }
 }
