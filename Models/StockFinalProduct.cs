5d0f0d995d4abe353de61831e8a49c3d030cfa00
Condesso1998 <ruicondesso98@gmail.com>
2022-12-30 00:58:56 +0000

    Model StockFinal

---
+++ Models/StockFinalProduct.cs
@@ -0,0 +1,15 @@
+﻿namespace CarManufactoring.Models
+{
+    public class StockFinalProduct
+    {
+        public int StockFinalProductId { get; set; }
+
+        public int LocalizationCarId { get; set; }
+        public LocalizationCar LocalizationCar { get; set; }
+
+        public string ChassiNumber { get; set; }
+
+        public int ProductionId { get; set; }
+        public Production Production { get; set; }
+    }
+}
