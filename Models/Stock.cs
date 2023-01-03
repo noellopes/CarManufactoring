0234ae2cf8ea758d0a686cf34c6adb2572ad5b16
LeonardoCMarques <47126387+LeonardoCMarques@users.noreply.github.com>
2023-01-03 15:47:02 +0000

    Fixing error of migrations

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -8,12 +8,16 @@ namespace CarManufactoring.Models
 
         [Required]
         [Display(Name = "Stock quantity")]
-        
+        //Quantidade tem de ser um valor double para calculo deste campo Ass:Grupo2
         public double Quantity { get; set; }
 
         [Required]
         [StringLength(30, MinimumLength = 3)]
         public string Location { get; set; }
+
+        public int CollaboratorId { get; set; }
+
+        public Collaborator? Collaborator { get; set; }
 
         public int MaterialId { get; set; }
         public Material? Material { get; set; }
