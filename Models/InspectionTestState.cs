afd7c4c50d425eb5cd7e819ee33000ed98505a50
TelmoMorais <telmolmorais01@gmail.com>
2023-01-03 16:11:15 +0000

    Criado modelo do estado do teste.

---
+++ Models/InspectionTestState.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarManufactoring.Models
+{
+    public class InspectionTestState
+    {
+        [Key]
+        public int InspectionTestStateId { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string State { get; set; }
+    }
+}
