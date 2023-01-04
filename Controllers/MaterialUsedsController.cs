dde6e6ac2a31391f91eb24e54c3a53e0af2c5a90
k7a-1 <k7a-1@AMEACA>
2023-01-04 14:05:45 +0000

    Alteracao no codigo de controller em MaterialUsed

--- Controllers/MaterialUsedsController.cs
+++ Controllers/MaterialUsedsController.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CarManufactoring.Data;
 using CarManufactoring.Models;
+using CarManufactoring.ViewModels;
+using System.Xml.Linq;
+using System.Security.Principal;
 
 namespace CarManufactoring.Controllers
 {
@@ -20,15 +23,53 @@ namespace CarManufactoring.Controllers
         }
 
         // GET: MaterialUseds
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int id)
         {
-              return View(await _context.MaterialUsed.ToListAsync());
+            if (id < 0) { return NotFound("Id Error"); }
+            //if (!MaterialUsedExists(id)) { return NotFound("This Semifinished does not exist in database yet"); }
+            try
+            {
+                // Selecionar as materialUsed(quantidades) para apresentar em primeiro
+                List<MaterialUsed> usedMaterials = _context.MaterialUsed.Select(mu => mu).Where(mu => mu.SemiFinishedId == id).OrderBy(mu => mu.Quantity).ToList();
+
+                // Selecionar todos os materiais (para listar-los)
+                List<Material> allMaterials = _context.Material.Select(m => m).OrderBy(m => m.Type).ToList();
+                if (allMaterials.Count == 0) { return NotFound("Empty Materials List Error"); }
+
+                // Criar um titulo com o nome do semiFinished obj
+                SemiFinished? semiFinished = _context.SemiFinished.Select(mu => mu).Where(mu => mu.SemiFinishedId == id).FirstOrDefault();
+                if (semiFinished == null) { return NotFound("SemiFinished not Found"); } // If semiFinished is not found
+
+                // Meter na ViewBag
+                ViewBag.SFName = string.Format("{0} ({1})", semiFinished.Family, semiFinished.Reference);
+                ViewBag.MaterialUseds = usedMaterials;
+                ViewBag.MaterialsAll = allMaterials;
+
+                // Returnar a View
+                return View("Index");
+            }
+            catch { return NotFound("Database Error"); }
         }
 
+        // GET: MaterialUseds/Edit
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    if (!MaterialUsedExists(id)) { return NotFound(); }
+        //    if (_context.MaterialUsed == null)
+        //    {
+        //        // Create a register
+        //        // Retornar em pagina de success create
+        //        // Pagina success create redireciona ao fim de 2/3 segundos para aqui
+        //    }
 
-        private bool MaterialUsedExists(int id)
-        {
-          return _context.MaterialUsed.Any(e => e.MaterialUsedId == id);
-        }
+        //    var materialUsed = await _context.MaterialUsed.FirstOrDefaultAsync(m => m.SemiFinishedId == id);
+        //    var semiFinished = await _context.SemiFinished.FirstOrDefaultAsync(m => m.SemiFinishedId == id);
+        //    var material = await _context.MaterialUsed.FirstOrDefaultAsync(m => m.MaterialUsedId == id);
+        //    if (materialUsed == null) { return View("Error"); }
+
+        //    return View("semiFinished/Index");
+        //}
+
+        private bool MaterialUsedExists(int id) => _context.MaterialUsed.Any(e => e.MaterialUsedId == id);
     }
 }
