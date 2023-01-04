49dded98967481ecf13fae460afc9383d6eb2195
Bruno Martins <brunomiguelmartins1522@gmail.com>
2023-01-04 23:13:48 +0000

    ViewModels

---
+++ ViewModels/Group2/WarehousesIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CarManufactoring.Models;
+
+namespace CarManufactoring.ViewModels.Group2
+{
+    public class WareHousesIndexViewModel
+    {
+        public ListViewModel<Warehouse> WarehousesList { get; set; }
+        public string? LocationSearch { get; set; }
+        public int? CollaboratorSearch { get; set; }
+
+    }
+}
