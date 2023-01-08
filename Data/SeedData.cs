6adc7df3150cd01cf67f78592028a8eeb2a70e5f
osmancanofraz <117988839+osmancanofraz@users.noreply.github.com>
2023-01-08 20:06:08 +0300

    edited proje

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -15,53 +15,6 @@ namespace CarManufactoring.Data
             
 
            
-            PopulateGender(db);
-            PopulateFunction(db);
-            PopulateCollaborators(db);
-            PopulateCarParts(db);
-            PopulateSemiFinisheds(db);
-            PopulateSemiFinishedCars(db);
-            PopulateMaterials(db);
-            PopulateSection(db);
-            PopulateSectionManager(db);
-            PopulateMachineState(db);
-            PopulateTaskType(db);
-            PopulateBrands(db);
-            PopulateInspectionTesting(db);
-            PopulateInspectionTestState(db);
-            PopulatePriority(db);
-            PopulateMachineBrand(db);
-            PopulateMachineModel(db);
-            PopulateLocalizationCode(db);
-            PopulateMachines(db);
-            PopulateMachineMaintenance(db);
-            PopulateCars(db);
-            PopulateTimeOfProduction(db);
-            PopulateCarConfigs(db);
-            PopulateShiftType(db);
-            PopulateShift(db);
-            PopulateCustomers(db);
-            PopulateCustomerContacts(db);
-            PopulateOrder(db);
-            PopulateMaterialUsed(db);
-            PopulateSupplier(db);
-            PopulateStocks(db);
-            PopulateWarehouseStocks(db);
-            PopulateExtras(db);
-            PopulateOrderState(db);
-            PopulateProductions(db);
-            PopulateWarehouses(db);
-            PopulateModelParts(db);
-            PopulateLocalizationCar(db);
-            PopulateStockFinalProduct(db);
-            PopulateLocalizationCar(db);
-            PopulateStockFinalProduct(db);
-            PopulateBreakdows(db);
-
-            PopulateSupplierPartsCarParts(db);
-            PopulateSupplierParts(db);
-
-
         }
         internal static async Task PopulateRolesAsync(RoleManager<IdentityRole> roleManager) {
             await EnsureRoleIsCreated(roleManager, "Admin");
--- Models/ShiftSchedule.cs
+++ Models/ShiftSchedule.cs
@@ -7,11 +7,11 @@ namespace CarManufactoring.Models
     {
         [Key]
         public int ShiftSchedulesId { get; set; }
-        [Required]
-        public string Name { get; set; }
         public DateTime StartDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; } = DateTime.Now;
         public string Duration { get; set; } = "";
+        [Required]
+        [Display(Name = "Name")]
         public int CollaboratorId { get; set; }
         public Collaborator? Collaborator { get; set; }
     }
